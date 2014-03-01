01c754390ab537fc4629563a6f579a4f93c35c53
sebv <seb.vincent@gmail.com>
2014-03-01 20:27:17 +0800

    adding missing scripts

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 var path = require('path');
-var bootstrap = path.resolve(__dirname, 'uiauto', 'bootstrap.js');
-module.exports = bootstrap;
+
+module.exports = 
+{ 
+    bootstrap: path.resolve(__dirname, 'uiauto', 'bootstrap.js'),
+    rotate: path.resolve(__dirname, "uiauto/Rotate.applescript")
+}
