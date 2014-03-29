5428aab6bc6f93c75f718376218609b4d1e31b78
sebv <seb.vincent@gmail.com>
2014-03-29 10:16:51 +0800

    better startup log

--- uiauto/lib/helpers.js
+++ uiauto/lib/helpers.js
@@ -6,8 +6,14 @@ var console = {
   log: function (msg) {
     if (typeof isVerbose !== "undefined" && isVerbose) UIALogger.logMessage(msg);
   },
+  warn: function (msg) {
+    UIALogger.logWarning("Warn: " + msg);
+  },
+  start: function (msg) {
+    UIALogger.logStart(msg);
+  },
   error: function (msg) {
-    UIALogger.logMessage("Error: " + msg);
+    UIALogger.logError("Error: " + msg);
   }
 };
 
