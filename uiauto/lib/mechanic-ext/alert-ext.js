e7709acfa1226bf294fc8eaae359f812374d90f1
scottdixon <scott@howrefreshing.com.au>
2015-09-25 14:57:56 -0700

    9.1 beta 2: Accept/dismiss fix

--- uiauto/lib/mechanic-ext/alert-ext.js
+++ uiauto/lib/mechanic-ext/alert-ext.js
@@ -53,7 +53,9 @@
       if (!alert.isNil()) {
         var acceptButton = alert.defaultButton();
         var buttonCount = alert.buttons().length;
-        if (acceptButton.isNil() && buttonCount > 0) {
+        // iOS9.1 returns 'cancel' as the default button.
+        if ((parseFloat($.systemVersion) >= 9.1 && buttonCount > 0) || 
+            (acceptButton.isNil() && buttonCount > 0)) {
           // last button is accept
           acceptButton = alert.buttons()[buttonCount - 1];
         }
